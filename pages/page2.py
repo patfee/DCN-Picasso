195cf7e7e786c0d6568d575ccd72dbd7882fbf80
patfee <patrick.feeleus@gmail.com>
2025-11-04 14:45:53 +0100

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,5 +1,4 @@
-from dash import html, dcc
-import dash_bootstrap_components as dbc
+from dash import html, dcc, callback, Input, Output
 from subpages import page2_tab_a
 
 layout = html.Div(
@@ -9,21 +8,16 @@ layout = html.Div(
             id="tabs-page2",
             value="page2-tab-a",
             children=[
-                dcc.Tab(label="Sub A", value="page2-tab-a"),
-                # You can add Sub B later if needed
+                dcc.Tab(label="Sub A", value="page2-tab-a"),  # Harbour Cdyn 1.15
+                # dcc.Tab(label="Sub B", value="page2-tab-b"),  # future
             ],
         ),
         html.Div(id="page2-tab-content"),
     ]
 )
 
-def render_tab_content(tab_value):
+@callback(Output("page2-tab-content", "children"), Input("tabs-page2", "value"))
+def _switch_tab(tab_value):
     if tab_value == "page2-tab-a":
         return page2_tab_a.layout
     return html.Div("Coming soon…")
-
-# Dash callback to switch tab content (this is usually in app.py or here if you wired it this way)
-from dash import callback, Input, Output
-@callback(Output("page2-tab-content", "children"), Input("tabs-page2", "value"))
-def _switch_tab(tab_value):
-    return render_tab_content(tab_value)
