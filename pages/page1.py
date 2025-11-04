fdf031c69d004610be54ef9dcf57d3b0cbc2087f
patfee <patrick.feeleus@gmail.com>
2025-11-04 10:03:25 +0100

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,5 +1,4 @@
-from dash import html, dcc
-import dash_bootstrap_components as dbc
+from dash import html, dcc, callback, Input, Output
 from subpages import page1_tab_a, page1_tab_b
 
 layout = html.Div(
@@ -13,20 +12,14 @@ layout = html.Div(
                 dcc.Tab(label="Sub B", value="tab-b"),
             ],
         ),
-        html.Div(id="tabs-content-page1")
+        html.Div(id="tabs-content-page1", className="mt-3"),
     ]
 )
 
-from dash import Input, Output
-from app import app
-
-@app.callback(
-    Output("tabs-content-page1", "children"),
-    Input("tabs-page1", "value")
-)
+@callback(Output("tabs-content-page1", "children"), Input("tabs-page1", "value"))
 def render_tab(tab):
     if tab == "tab-a":
         return page1_tab_a.layout
-    elif tab == "tab-b":
+    if tab == "tab-b":
         return page1_tab_b.layout
     return html.Div("Select a tab")
